785274978973acc62fd3d0d5fc705c88e11b842d
Venkatesh <venkatesh.kamalapathy@analog.com>
2021-11-23 19:29:46 +0530

    POST Message mutation

--- src/server.js
+++ src/server.js
@@ -10,12 +10,30 @@ const typeDefs = `
   type Query {
       messages: [Message!]
   }
+
+  type Mutation {
+    postMessage( user: String!, message: String!): ID!
+  }
 `
 const messages = [];
 const resolvers = {
+  
   Query: {
     messages: () => messages,
   },
+
+  Mutation: {
+    postMessage: (parent, {user, message}) => {
+      const id = messages.length;
+      messages.push({
+        id,
+        user,
+        message
+      });
+      return id;
+    },
+    
+  }
 }
 
 const server = new GraphQLServer({ typeDefs, resolvers })
